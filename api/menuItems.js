8cf4fe9da6a3c1652172001086d57a72d9973a31
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 17:29:07 +0100

    Create GET and POST routes in menuItems route

--- api/menuItems.js
+++ api/menuItems.js
@@ -6,4 +6,51 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
 
+menuItemsRouter.get("/", (req, res, next) => {
+  db.all(
+    `SELECT * FROM MenuItem WHERE menu_id = ${req.params.menuId}`,
+    (err, menuItems) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({ menuItems: menuItems });
+      }
+    }
+  );
+});
+
+menuItemsRouter.post("/", (req, res, next) => {
+  const { name, description, inventory, price } = req.body.menuItem;
+  const menuId = req.params.menuId;
+  if (!name || !inventory || !price) {
+    return res.sendStatus(400);
+  }
+  db.run(
+    `INSERT INTO MenuItem (name, description, inventory, price, menu_id) VALUES ($name, $description, $inventory, $price, $menuId)`,
+    {
+      $name: name,
+      $description: description,
+      $inventory: inventory,
+      $price: price,
+      $menuId: menuId,
+    },
+    function (err) {
+      if (err) {
+        res.sendStatus(404);
+      } else {
+        db.get(
+          `SELECT * FROM MenuItem WHERE id = ${this.lastID}`,
+          (err, menuItem) => {
+            if (err) {
+              next(err);
+            } else {
+              res.status(201).json({ menuItem: menuItem });
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
 module.exports = menuItemsRouter;
