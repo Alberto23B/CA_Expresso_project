90aedb4927c731567850be28dba1b7a4f1b6fd93
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 15:46:01 +0100

    Create  GET and PUT route at :menuId in menu route

--- api/menus.js
+++ api/menus.js
@@ -7,6 +7,19 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || "./database.sqlite"
 );
 
+menusRouter.param("menuId", (req, res, next, menuId) => {
+  db.get(`SELECT * FROM Menu WHERE Menu.id = ${menuId}`, (err, menu) => {
+    if (err) {
+      next(err);
+    } else if (menu) {
+      req.menu = menu;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
 menusRouter.get("/", (req, res, next) => {
   db.all(`SELECT * FROM Menu`, (err, menus) => {
     if (err) {
@@ -35,6 +48,39 @@ menusRouter.post("/", (req, res, next) => {
             next(err);
           } else {
             res.status(201).json({ menu: menu });
+          }
+        });
+      }
+    }
+  );
+});
+
+menusRouter.get("/:menuId", (req, res, next) => {
+  db.get(`SELECT * FROM Menu WHERE id = ${req.params.menuId}`, (err, menu) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({ menu: menu });
+    }
+  });
+});
+
+menusRouter.put("/:menuId", (req, res, next) => {
+  const { title } = req.body.menu;
+  if (!title) {
+    return res.sendStatus(400);
+  }
+  db.run(
+    `UPDATE Menu SET title = "${title}" WHERE id = ${req.params.menuId}`,
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        db.get(`SELECT * FROM Menu WHERE id = ${req.menu.id}`, (err, menu) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(200).json({ menu: menu });
           }
         });
       }
