f77c35c9f3fa363f751c1852b14f0575ac31ee83
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 10:10:23 +0100

    Setup server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("errorhandler");
+const morgan = require("morgan");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+app.use(errorHandler());
+app.use(morgan("dev"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+module.exports = app;
