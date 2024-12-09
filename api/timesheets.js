3e0d5dca8977d2a021d53e3adf3cdbc45b2990d4
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 14:43:07 +0100

    Create GET and POST routes at "/api/employees/:employeeId/timesheets"

--- api/timesheets.js
+++ api/timesheets.js
@@ -1,4 +1,55 @@
 const express = require("express");
+const sqlite3 = require("sqlite3");
 const timesheetsRouter = express.Router({ mergeParams: true });
+
+const db = new sqlite3.Database(
+  process.env.TEST_DATABASE || "./database.sqlite"
+);
+
+timesheetsRouter.get("/", (req, res, next) => {
+  db.all(
+    `SELECT * FROM Timesheet WHERE employee_id = ${req.employee.id}`,
+    (err, timesheets) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({ timesheets: timesheets });
+      }
+    }
+  );
+});
+
+timesheetsRouter.post("/", (req, res, next) => {
+  const { hours, rate, date } = req.body.timesheet;
+  const employeeId = req.employee.id;
+  if (!hours || !rate || !date) {
+    return res.sendStatus(400);
+  }
+  db.run(
+    `INSERT INTO Timesheet (hours, rate, date, employee_id) VALUES ($hours, $rate, $date, $employeeId)`,
+    {
+      $hours: hours,
+      $rate: rate,
+      $date: date,
+      $employeeId: employeeId,
+    },
+    function (err) {
+      if (err) {
+        res.sendStatus(404);
+      } else {
+        db.get(
+          `SELECT * FROM Timesheet WHERE id = ${this.lastID}`,
+          (err, timesheet) => {
+            if (err) {
+              next(err);
+            } else {
+              res.status(201).json({ timesheet: timesheet });
+            }
+          }
+        );
+      }
+    }
+  );
+});
 
 module.exports = timesheetsRouter;
