c089e459365ec82e04b8ebf9b366605ea7eea18f
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 15:35:35 +0100

    Create POST route in Menu

--- api/menus.js
+++ api/menus.js
@@ -17,4 +17,29 @@ menusRouter.get("/", (req, res, next) => {
   });
 });
 
+menusRouter.post("/", (req, res, next) => {
+  const { title } = req.body.menu;
+  if (!title) {
+    return res.sendStatus(400);
+  }
+  db.run(
+    `INSERT INTO Menu (title) VALUES (
+    "${title}"
+    )`,
+    function (err) {
+      if (err) {
+        next(err);
+      } else {
+        db.get(`SELECT * FROM Menu WHERE id = ${this.lastID}`, (err, menu) => {
+          if (err) {
+            next(err);
+          } else {
+            res.status(201).json({ menu: menu });
+          }
+        });
+      }
+    }
+  );
+});
+
 module.exports = menusRouter;
