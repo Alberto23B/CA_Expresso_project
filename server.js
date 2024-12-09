81306348dcad57fe5143cb96dbd68dd6b101a199
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-09 10:28:04 +0100

    setup apiRouter

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require("errorhandler");
 const morgan = require("morgan");
+const apiRouter = require("./api/api");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use(errorHandler());
 app.use(morgan("dev"));
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
